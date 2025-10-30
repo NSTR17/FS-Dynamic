1c8c3b5fdb2f112d070a7b76af21d3a0fb941fa3
NSTR17 <parmanin170689@gmail.com>
2025-10-30 23:56:43 +0300

    1. Обновлен параметр Startup 2. Добавлены окна по ролям

--- FS Dynamic/App.xaml.cs
+++ FS Dynamic/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using FS_Dynamic.Models;
 
 namespace FS_Dynamic
 {
@@ -13,5 +14,52 @@ namespace FS_Dynamic
     /// </summary>
     public partial class App : Application
     {
+        /// <summary>
+        /// Обработчик запуска приложения - вызывается ПЕРВЫМ при старте
+        /// Здесь решаем какое окно открывать
+        /// </summary>
+        /// 
+        private void Application_Startup(object sender, StartupEventArgs e)
+        { 
+            var loginWindow = new LoginWindow();
+            bool? loginResult = loginWindow.ShowDialog();
+
+            if (loginResult == true && loginWindow.CurrentUser != null)
+            {
+                OpenMainWindowBasedOnRole(loginWindow.CurrentUser);
+            }
+            else
+            { 
+                Current.Shutdown();
+            }
+        }
+
+        /// <summary>
+        /// Открывает главное окно в зависимости от роли пользователя
+        /// </summary>
+        /// <param name="user">Авторизованный пользователь</param>
+        /// 
+
+        private void OpenMainWindowBasedOnRole(User user)
+        {
+            Window mainWindow;
+
+            switch (user.Role.ToLower())
+            {
+                case "admin":
+                    mainWindow = new MainWindow(user);
+                    break;
+                case "operator":
+                    mainWindow = new OperatorWindow(user);
+                    break;
+                case "athlete":
+                    mainWindow = new AthleteWindow(user);
+                default:
+                    MessageBox.Show($"Неизвестная роль: {user.Role}", "Ошибка");
+                    Current.Shutdown();
+                    return;
+            }
+            mainWindow.Show();
+        }
     }
 }
